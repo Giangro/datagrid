ba308db8198f9a01614a95a7df697bcdabde4fe9
alex <alessandro.giangreco@gmail.com>
2022-10-12 15:12:57 +0200

    First Commit

---
+++ src/test/java/it/poste/hazelcast/datagrid/DatagridApplicationTests.java
@@ -0,0 +1,13 @@
+package it.poste.hazelcast.datagrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatagridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
