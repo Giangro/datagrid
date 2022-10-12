15bc790ddbb347c77e281252ae2c5c806adac1b2
alex <alessandro.giangreco@gmail.com>
2022-10-13 00:06:57 +0200

    add logging

---
+++ src/main/java/it/poste/hazelcast/datagrid/config/Config.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.poste.hazelcast.datagrid.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author alex
+ */
+@Configuration
+@Slf4j
+public class Config {    
+}
--- src/main/java/it/poste/hazelcast/datagrid/controller/DataGridController.java
+++ src/main/java/it/poste/hazelcast/datagrid/controller/DataGridController.java
@@ -4,6 +4,7 @@
  */
 package it.poste.hazelcast.datagrid.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author alex
  */
 @RestController
+@Slf4j
 public class DataGridController {
     
     @GetMapping("/echo")
     public String get(@RequestParam(value = "message") String message) {       
+        log.info("calling rest controller for echo: message = \"{}\"",message);
         return message;
     }
     
