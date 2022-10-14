60b374254a4efd6618ca064f3de5ebaca03eb547
giangr40 <alessandro.giangreco@posteitaliane.it>
2022-10-14 19:49:45 +0200

    skip test during deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-/usr/bin/mvn spring-boot:build-image -DskipTests
+./mvnw spring-boot:build-image -DskipTests
 docker tag datagrid:latest localhost:5000/datagrid:latest
 docker push localhost:5000/datagrid:latest
 kubectl delete deployments datagrid-deployment -n develop
