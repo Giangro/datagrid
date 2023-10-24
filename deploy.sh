f96ff466134f1e6fb7a919c1265fd5a8fc372b47
giangro <alessandro.giangreco@gmail.com>
2023-10-24 21:24:13 +0200

    docker desktop version w/nginx ingress controller

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-./mvnw spring-boot:build-image -DskipTests
+mvn spring-boot:build-image -DskipTests
 docker tag datagrid:latest localhost:5000/datagrid:latest
 docker push localhost:5000/datagrid:latest
 kubectl delete deployments datagrid-deployment -n develop
